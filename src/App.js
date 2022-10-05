4a30bb17678a22bcc16143b4fc40670ca541c6fa
Turchanov Denis <turchanovdenis786@gmail.com>
2022-10-05 23:41:17 +0200

    add user-bar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './css/App.css';
 
 function App() {
   return (
-    <div className= "wrapper">
+    <div className="wrapper">
       <header className="header">
         <div className="header__body">
           <div className="header__column">
@@ -21,6 +21,18 @@ function App() {
           </div>
           <div className="header__column">
             {/* UserBar component */}
+            <div className="user-bar">
+              <div className="user-bar__body">
+                <div className="user-bar__column"><div className="user-bar__trash trash">
+                  <div className="trash">
+                    <a href="" className="trash__icon"> <img src="/img/user-bar-icon/trash.svg"></img></a>
+                    <div className="trash__count">100p</div>
+                  </div>
+                </div></div>
+                <div className="user-bar__column"><div className="user-bar__favorite-item"><img src="/img/user-bar-icon/favorite.svg"></img></div></div>
+                <div className="user-bar__column"><div className="user-bar__profile"><img src="/img/user-bar-icon/user-profile.svg"></img></div></div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
