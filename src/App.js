da306a8f0b26977148c96544af6707da7dfe3ec8
Turchanov Denis <turchanovdenis786@gmail.com>
2022-10-05 23:14:00 +0200

    create header logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
-    <div></div>
+    <div className= "wrapper">
+      <header className="header">
+        <div className="header__body">
+          <div className="header__column">
+            {/* Logo component */}
+            <div className="logo">
+              <div className='logo__body'>
+                <div className="logo__column">
+                  <div className="logo__icon"> <img src="/img/logo.png"></img></div>
+                </div>
+                <div className="logo__column">
+                  <div className="logo__title">REACT SNEAKERS</div>
+                  <div className="logo__subtitle">Магазин лучших кроссовок</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="header__column">
+            {/* UserBar component */}
+          </div>
+        </div>
+      </header>
+    </div>
   );
 }
 
