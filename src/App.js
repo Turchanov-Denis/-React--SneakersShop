6fd08e4cc0f597b7014876f0398847f5b3caedd8
Turchanov Denis <turchanovdenis786@gmail.com>
2022-10-06 02:00:58 +0200

    create sneaker-item layout
    - html layout
    - css style
    - tested in jsx

--- src/App.js
+++ src/App.js
@@ -51,6 +51,20 @@ function App() {
                     <div className="content__title">Все кроссовки</div>
                     <input className="content__find-input" placeholder='Поиск...'></input>
                 </div>
+                <div className="sneaker-item">
+                    <button className="sneaker-item__favorite-icon ibg"></button>
+                    <div className="sneaker-item__image ibg" style={{backgroundImage: "url('/img/content/sneakers/sneaker_01.png')"}}></div>
+                    <div className="sneaker-item__title">
+                    <p>Мужские Кроссовки</p>
+                    <p>Nike Blazer Mid Suede</p></div>
+                    <div className="sneaker-item__block">
+                        <div className="sneaker-item__price">
+                            <p>Price</p>
+                            <span>12 999 руб.</span>
+                        </div>
+                        <button className="sneaker-item__add-icon ibg"></button>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
