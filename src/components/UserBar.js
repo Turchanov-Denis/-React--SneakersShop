c8550993c0104e947617dea31aef7d6e8c96cfa5
Turchanov Denis <turchanovdenis786@gmail.com>
2022-10-09 23:52:55 +0200

    update style UserBar

--- src/components/UserBar.js
+++ src/components/UserBar.js
@@ -8,12 +8,12 @@ export default function UserBar({ counter, onTrashClick }) {
       <div className="user-bar__body">
         <div className="user-bar__column"><div className="user-bar__trash trash">
           <div className="trash">
-            <button className="trash__icon" onClick={onTrashClick}> <img src="/img/user-bar-icon/trash.svg" ></img><div className="trash__count">{counter}p</div></button>
+            <button className="trash__icon" onClick={onTrashClick}> <img src="/img/user-bar-icon/trash.svg" alt="shopCart"></img><div className="trash__count">{counter}p</div></button>
 
           </div>
         </div></div>
-        <div className="user-bar__column"><div className="user-bar__favorite-item"><img src="/img/user-bar-icon/favorite.svg"></img></div></div>
-        <div className="user-bar__column"><div className="user-bar__profile"><img src="/img/user-bar-icon/user-profile.svg"></img></div></div>
+        <div className="user-bar__column"><button className="user-bar__favorite-item"><img src="/img/user-bar-icon/favorite.svg" alt="favorites"></img></button></div>
+        <div className="user-bar__column"><button className="user-bar__profile"><img src="/img/user-bar-icon/user-profile.svg" alt="profile"></img></button></div>
       </div>
     </div>
   )
