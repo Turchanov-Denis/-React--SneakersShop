2a8621d5e271aa671181e546df3a2a0ff79e6861
Turchanov Denis <turchanovdenis786@gmail.com>
2022-10-10 20:52:01 +0200

    step 1

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,49 +2,29 @@ import React from "react";
 import './Card.css';
 import AppContext from "../AppContext";
 function Card(props) {
-  const { isAdded } = React.useContext(AppContext)
-  // state = {
-  //   isToggleOnAdd: isAdded,
-  //   isToggleOnFavorite: props.isFavorite,
-  // };
-  url = "url('" + props.item.url + "')";
-  handleClickFavorite = handleClickFavorite.bind(this);
-  handleClickAdd = handleClickAdd.bind(this)
+  const { isAdded, isFavorite } = React.useContext(AppContext)
+  const url = "url('" + props.item.url + "')";
 
 
-  handleClickAdd() {
-    setState(prevState => ({
-      isToggleOnAdd: !prevState.isToggleOnAdd
-    }));
-    state.isToggleOnAdd ? props.onRepeatClick(props.item) : props.onPlus(props.item)
-    // render()
-  }
-  handleClickFavorite() {
-    setState(prevState => ({
-      isToggleOnFavorite: !prevState.isToggleOnFavorite
-    }));
-    state.isToggleOnFavorite ? props.onRepeatClickFavorite(props.item) : props.onFavorite(props.item)
-    // render()
-  }
-  render() {
-    return (
-      <div className="сard">
-        <button className={(state.isToggleOnFavorite) ? "сard__favorite-icon_active ibg " : "сard__favorite-icon ibg "} onClick={handleClickFavorite}></button>
-        <div className="сard__image ibg" style={{ backgroundImage: url }}></div>
-        <div className="сard__title">
-          {props.item.title}</div>
-        <div className="сard__block">
-          <div className="сard__price">
-            <p>Price</p>
-            <span>{props.item.price} руб.</span>
-          </div>
-          <button className={isAdded ? "сard__add-icon_active ibg " : "сard__add-icon ibg "} onClick={handleClickAdd}></button>
+
+  return (
+    <div className="сard">
+      <button className={(isFavorite) ? "сard__favorite-icon_active ibg " : "сard__favorite-icon ibg "} onClick={() => { isAdded ? props.onRepeatClickFavorite(props.item) : props.onFavorite(props.item) }}></button>
+      <div className="сard__image ibg" style={{ backgroundImage: url }}></div>
+      <div className="сard__title">
+        {props.item.title}</div>
+      <div className="сard__block">
+        <div className="сard__price">
+          <p>Price</p>
+          <span>{props.item.price} руб.</span>
         </div>
+        <button className={isAdded ? "сard__add-icon_active ibg " : "сard__add-icon ibg "} onClick={() => isAdded ? props.onRepeatClick(props.item) : props.onPlus(props.item)}></button>
       </div>
-    )
-  }
-
+    </div >
+  )
 }
+
+
 
 export default Card;
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,23 @@
 import Card from "../components/Card"
 import CartLoder from "../components/CartLoader"
 import AppContext from "../components/AppContext"
-function Home({dataSneaker,
-    onChangeInput,
-    searchValue,
-    trashSneaker,
-    addToFavorite,
-    addToShoppingCart,
-    removeToFavorite,
-    removeToShoppingCart,
-    favoriteSneaker,isLoading = true}){
+function Home({ dataSneaker,
+  onChangeInput,
+  searchValue,
+  trashSneaker,
+  addToFavorite,
+  addToShoppingCart,
+  removeToFavorite,
+  removeToShoppingCart,
+  favoriteSneaker, isLoading = true }) {
 
-      const isAdded = (id)=> {trashSneaker.some(e => Number(e.id) === Number(id) )}
+  const isAdded = (id) => { trashSneaker.some(e => Number(e.id) === Number(id)) }
+  const isFavorite = (id) => { favoriteSneaker.some(e => Number(e.id) === Number(id)) }
 
 
-    return (
-      <AppContext.Provider value={isAdded}>
-          <section className="content">
+  return (
+    <AppContext.Provider value={{isAdded, isFavorite}}>
+      <section className="content">
         <div className="container">
           <div className="content__header">
             <div className="content__title">All Sneakers</div>
@@ -25,20 +26,20 @@ function Home({dataSneaker,
           {/* item */}
           <div className='content__body'>
             {/* {createContent(dataSneaker)} */}
-            
 
-            {isLoading ? (<><CartLoder></CartLoder><CartLoder></CartLoder><CartLoder></CartLoder><CartLoder></CartLoder><CartLoder></CartLoder><CartLoder></CartLoder></>): (dataSneaker.filter(item => item.title.toUpperCase().includes(searchValue.toUpperCase())).map((item) =>
-            (<div className='content__column'><Card  key={item.id} item={item}  onFavorite={(obj) => { addToFavorite(obj) }} onPlus={(obj) => {
-              addToShoppingCart(obj) 
+
+            {isLoading ? (<><CartLoder></CartLoder><CartLoder></CartLoder><CartLoder></CartLoder><CartLoder></CartLoder><CartLoder></CartLoder><CartLoder></CartLoder></>) : (dataSneaker.filter(item => item.title.toUpperCase().includes(searchValue.toUpperCase())).map((item) =>
+            (<div className='content__column'><Card key={item.id} item={item} onFavorite={(obj) => { addToFavorite(obj) }} onPlus={(obj) => {
+              addToShoppingCart(obj)
             }} onRepeatClickFavorite={(obj) => { removeToFavorite(obj) }} onRepeatClick={(obj) => {
               removeToShoppingCart(obj)
-            }}  isFavorite={favoriteSneaker.some(e => Number(e.id) === Number(item.id) )}></Card></div>)
+            }} isFavorite={favoriteSneaker.some(e => Number(e.id) === Number(item.id))}></Card></div>)
             ))}
           </div>
         </div>
       </section>
-      </AppContext.Provider>
-    )
+    </AppContext.Provider>
+  )
 }
 
 export default Home
