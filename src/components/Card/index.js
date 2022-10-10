694950eb0855be1484698c0fc652464d06cd6370
Turchanov Denis <turchanovdenis786@gmail.com>
2022-10-10 21:25:29 +0200

    path of hero : realize - destructive props for Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,27 +1,25 @@
 import React from "react";
 import './Card.css';
 import AppContext from "../AppContext";
-function Card(props) {
+
+function Card({ item, onRepeatClick, onRepeatClickFavorite, onFavorite, onPlus }) {
   const { isAdded, isFavorite } = React.useContext(AppContext)
-  const url = "url('" + props.item.url + "')";
- 
-
-
-
+  const url = "url('" + item.url + "')";
+  console.log(url)
   return (
-    <div className="сard">
-      <button className={isFavorite(props.item.id) ? "сard__favorite-icon_active ibg " : "сard__favorite-icon ibg "} onClick={() => { isFavorite(props.item.id) ? props.onRepeatClickFavorite(props.item) : props.onFavorite(props.item) }}></button>
-      <div className="сard__image ibg" style={{ backgroundImage: url }}></div>
-      <div className="сard__title">
-        {props.item.title}</div>
-      <div className="сard__block">
-        <div className="сard__price">
+    <div className="card">
+      <button className={isFavorite(item.id) ? "card__favorite-icon_active ibg " : "card__favorite-icon ibg "} onClick={() => { isFavorite(item.id) ? onRepeatClickFavorite(item) : onFavorite(item) }}></button>
+      <div className="card__image ibg" style={{ backgroundImage: url }}></div>
+      <div className="card__title">
+        {item.title}</div>
+      <div className="card__block">
+        <div className="card__price">
           <p>Price</p>
-          <span>{props.item.price} руб.</span>
+          <span>{item.price} руб.</span>
         </div>
-        <button className={isAdded(props.item.id) ? "сard__add-icon_active ibg " : "сard__add-icon ibg "} onClick={() => isAdded(props.item.id) ? props.onRepeatClick(props.item) :  props.onPlus(props.item)}></button>
+        <button className={isAdded(item.id) ? "card__add-icon_active ibg " : "card__add-icon ibg "} onClick={() => isAdded(item.id) ? onRepeatClick(item) : onPlus(item)}></button>
       </div>
-    </div >
+    </div>
   )
 }
 
